471d85adf2bf45703b144e4d308be42af96e9cc6
plotnikovaya <yana.plotnikova@nixsolutions.com>
2022-06-06 16:40:32 +0300

    checkboxes homework update

--- src/test/java/TestNGDemo.java
+++ src/test/java/TestNGDemo.java
@@ -3,6 +3,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
@@ -13,22 +14,10 @@ public class TestNGDemo {
     private By javaCheckboxLocator = By.xpath("//*[@id=\"contentblock\"]/section/div[1]/input[1]");
     private By seleniumCheckboxLocator = By.xpath("//*[@id=\"contentblock\"]/section/div[2]/input");
     private By iAmSelectedCheckboxLocator = By.xpath("//*[@id=\"contentblock\"]/section/div[3]/input[2]");
-    private By optionOneCheckboxLocator = By.xpath("//*[@id=\"contentblock\"]/section/div[4]/input[1]");
-    private By optionTwoCheckboxLocator = By.xpath("//*[@id=\"contentblock\"]/section/div[4]/input[2]");
-    private By optionThreeCheckboxLocator = By.xpath("//*[@id=\"contentblock\"]/section/div[4]/input[3]");
-    private By optionFourCheckboxLocator = By.xpath("//*[@id=\"contentblock\"]/section/div[4]/input[4]");
-    private By optionFiveCheckboxLocator = By.xpath("//*[@id=\"contentblock\"]/section/div[4]/input[5]");
-    private By optionSixCheckboxLocator = By.xpath("//*[@id=\"contentblock\"]/section/div[4]/input[6]");
     private WebDriver driver;
     private WebElement javaCheckbox;
     private WebElement seleniumCheckbox;
     private WebElement iAmSelectedCheckbox;
-    private WebElement optionOneCheckbox;
-    private WebElement optionTwoCheckbox;
-    private WebElement optionThreeCheckbox;
-    private WebElement optionFourCheckbox;
-    private WebElement optionFiveCheckbox;
-    private WebElement optionSixCheckbox;
 
     @BeforeClass
     public void setUp() {
@@ -38,12 +27,6 @@ public class TestNGDemo {
         javaCheckbox = driver.findElement(javaCheckboxLocator);
         seleniumCheckbox = driver.findElement(seleniumCheckboxLocator);
         iAmSelectedCheckbox = driver.findElement(iAmSelectedCheckboxLocator);
-        optionOneCheckbox = driver.findElement(optionOneCheckboxLocator);
-        optionTwoCheckbox = driver.findElement(optionTwoCheckboxLocator);
-        optionThreeCheckbox = driver.findElement(optionThreeCheckboxLocator);
-        optionFourCheckbox = driver.findElement(optionFourCheckboxLocator);
-        optionFiveCheckbox = driver.findElement(optionFiveCheckboxLocator);
-        optionSixCheckbox = driver.findElement(optionSixCheckboxLocator);
     }
 
     @Test
@@ -72,4 +55,9 @@ public class TestNGDemo {
             Assert.assertTrue(checkbox.isSelected());
         }
     }
+    @AfterClass
+    public void tearDown() {
+        driver.close();
+    }
+
 }
